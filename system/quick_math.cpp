bbad765b228c0f186f9a332f980ea129dcf4a9f9
Alexey Vasyukov <vasyukov@gmail.com>
2012-03-24 11:47:45 +0400

    Add vector_product and same_orientation functions into quick_math class

--- system/quick_math.cpp
+++ system/quick_math.cpp
@@ -30,6 +30,32 @@ float quick_math::scalar_product(float x1, float y1, float z1, float x2, float y
 	return x1*x2 + y1*y2 + z1*z2;
 };
 
+void quick_math::vector_product(float x1, float y1, float z1, float x2, float y2, float z2, float *nx, float *ny, float *nz)
+{
+	*nx = y1 * z2 - y2 * z1;
+	*ny = x2 * z1 - x1 * z2;
+	*nz = x1 * y2 - x2 * y1;
+};
+
+bool quick_math::same_orientation(float* base1, float* base2, float* check1, float* check2)
+{
+	// tmp normals used to check tri orientation
+	float n1[3];
+	float n2[3];
+
+	vector_product( 	base1[0] - check1[0], 	base1[1] - check1[1], 	base1[2] - check1[2],
+				base2[0] - check1[0], 	base2[1] - check1[1], 	base2[2] - check1[2],
+				&n1[0], 		&n1[1], 		&n1[2] 			);
+
+	vector_product( 	base1[0] - check2[0], 	base1[1] - check2[1], 	base1[2] - check2[2],
+				base2[0] - check2[0], 	base2[1] - check2[1], 	base2[2] - check2[2],
+				&n2[0], 		&n2[1], 		&n2[2]	 		);
+
+	if( scalar_product(n1[0], n1[1], n1[2], n2[0], n2[1], n2[2]) <= 0)
+		return false;
+	return true;
+};
+
 // Norm of the vector
 float quick_math::vector_norm(float x1, float y1, float z1)
 {
--- system/quick_math.h
+++ system/quick_math.h
@@ -10,6 +10,8 @@ public:
 	float tri_area(float x1, float y1, float z1, float x2, float y2, float z2);
 	float determinant(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3);
 	float scalar_product(float x1, float y1, float z1, float x2, float y2, float z2);
+	void vector_product(float x1, float y1, float z1, float x2, float y2, float z2, float *nx, float *ny, float *nz);
+	bool same_orientation(float* base1, float* base2, float* check1, float* check2);
 	float vector_norm(float x1, float y1, float z1);
 	float solid_angle(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3);
 	static const float PI;
