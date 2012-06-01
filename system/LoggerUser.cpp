76e26aff58957bce48c2f92f30d6b3764a45e891
Alexey Ermakov <fufler@gmail.com>
2012-06-01 20:27:28 +0400

    mend

---
+++ system/LoggerUser.cpp
@@ -0,0 +1,15 @@
+/* 
+ * File:   LoggerUser.cpp
+ * Author: lx
+ * 
+ * Created on 1 Июнь 2012 г., 16:49
+ */
+
+#include "LoggerUser.h"
+#include "Logger.h"
+
+LoggerUser::LoggerUser() {
+	logger = Logger::getInstace();
+}
+
+
