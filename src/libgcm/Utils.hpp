510e2e48246eed0622606c04d83aac3472420c98
Alexey Ermakov <fufler@gmail.com>
2014-04-02 16:25:42 +0400

    Replaced t_to_string with std::to_string

--- src/libgcm/Utils.hpp
+++ src/libgcm/Utils.hpp
@@ -17,14 +17,4 @@ void checkStream(fstream &f);
 
 void replaceAll(std::string& str, const std::string& from, const std::string& to);
 
-template<class T>
-static std::string t_to_string(T i)
-{
-    std::stringstream ss;
-    std::string s;
-    ss << i;
-    s = ss.str();
-    return s;
-}
-
 #endif
--- src/libgcm/snapshot/SnapshotWriter.cpp
+++ src/libgcm/snapshot/SnapshotWriter.cpp
@@ -6,8 +6,8 @@ gcm::SnapshotWriter::~SnapshotWriter() {
 
 string gcm::SnapshotWriter::getFileName(int cpuNum, int step, string meshId) {
     string filename = fname;
-    replaceAll (filename, "%z", t_to_string (cpuNum));
-    replaceAll (filename, "%n", t_to_string (step));
+    replaceAll (filename, "%z", to_string (cpuNum));
+    replaceAll (filename, "%n", to_string (step));
     replaceAll (filename, "%m", meshId);
     return filename;
 }
