e9f242bf7d54d9440b4a1a83d7a062edb7fe7878
Alexey Vasyukov <vasyukov@gmail.com>
2014-05-09 11:34:40 +0400

    Update materials handling in loader: - Check if the material is used in task or not - Check if target plasticity properties are present - Logging takes into account both checks mentioned above

--- src/launcher/launcher.hpp
+++ src/launcher/launcher.hpp
@@ -19,6 +19,8 @@ namespace launcher {
 		Area* readBoxArea(xml::Node areaNode);
 		Area* readArea(xml::Node areaNode);
 
+        std::vector<int> usedMaterialsIds;
+
         USE_LOGGER;
     public:
         Launcher();
