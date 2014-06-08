cf4abac0ff33d215dde3f1a9ed74b3c9b6d03f61
Alexey Vasyukov <vasyukov@gmail.com>
2014-06-08 20:02:09 +0400

    Fixes and hacks to make anisotropic delamination working

--- src/libgcm/method/InterpolationFixedAxis.cpp
+++ src/libgcm/method/InterpolationFixedAxis.cpp
@@ -82,8 +82,9 @@ void gcm::InterpolationFixedAxis::doNextPartStep(CalcNode& cur_node, CalcNode& n
         // FIXME_ASAP - do smth with this!
         // It is not stable now. See ugly hack below.
         // Think about: (a) cube, (b) rotated cube, (c) sphere.
-        // outer_normal[0] = outer_normal[1] = outer_normal[2] = 0;
-        // outer_normal[stage] = 1;
+        float val = (outer_normal[stage] >= 0 ? 1.0 : -1.0);
+        outer_normal[0] = outer_normal[1] = outer_normal[2] = 0;
+        outer_normal[stage] = val;
         // If there is no 'outer' omega - it is ok, border node can be inner for some directions
         if (outer_count == 0)
         {
@@ -121,7 +122,7 @@ void gcm::InterpolationFixedAxis::doNextPartStep(CalcNode& cur_node, CalcNode& n
                 // TODO FIXME - most probably CollisionDetector should do it
                 // But we should check it anycase
                 virt_node.setInContact(true);
-                virt_node.contactNodeNum = cur_node.contactNodeNum;
+                //virt_node.contactNodeNum = cur_node.contactNodeNum;
 
                 // Variables used in calculations internally
 
