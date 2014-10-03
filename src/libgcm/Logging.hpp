173e464af61074bebe08749579939d4d392ba2f6
Alexey Ermakov <fufler@gmail.com>
2014-10-03 10:46:12 +0400

    Added an option to completely disable certain logging levels at compile time

--- src/libgcm/Logging.hpp
+++ src/libgcm/Logging.hpp
@@ -1,32 +1,57 @@
 #ifndef GCM_LOGGING_H_
 #define GCM_LOGGING_H_
 
+#include "libgcm/util/Macros.hpp"
 #include "libgcm/config.hpp"
 
 #if CONFIG_ENABLE_LOGGING
     #include <log4cxx/logger.h>
     #include <log4cxx/mdc.h>
     #define USE_LOGGER log4cxx::LoggerPtr logger;
-    #define INIT_LOGGER(name) do { logger = log4cxx::Logger::getLogger(name); } while (0)
+    #define INIT_LOGGER(name) logger = log4cxx::Logger::getLogger(name)
     #define USE_AND_INIT_LOGGER(name) log4cxx::LoggerPtr logger = log4cxx::Logger::getLogger(name);
-
-    #define LOG_INFO(message)  do { LOG4CXX_INFO (logger, message) } while (0)
-    #define LOG_WARN(message)  do { LOG4CXX_WARN (logger, message) } while (0)
-    #define LOG_FATAL(message) do { LOG4CXX_FATAL(logger, message) } while (0)
-    #define LOG_ERROR(message) do { LOG4CXX_ERROR(logger, message) } while (0)
-    #define LOG_TRACE(message) do { LOG4CXX_TRACE(logger, message) } while (0)
-    #define LOG_DEBUG(message) do { LOG4CXX_DEBUG(logger, message) } while (0)
 #else
     #define USE_LOGGER ;
-    #define USE_AND_INIT_LOGGER(name);
-    #define INIT_LOGGER(name) do {} while (0)
+    #define USE_AND_INIT_LOGGER(name) DO_ONCE(;)
+    #define INIT_LOGGER(name) DO_ONCE(;)
+    #define USE_AND_INIT_LOGGER(name) DO_ONCE(;)
+#endif
 
-    #define LOG_INFO(message)  do {} while (0)
-    #define LOG_WARN(message)  do {} while (0)
-    #define LOG_FATAL(message) do {} while (0)
-    #define LOG_ERROR(message) do {} while (0)
-    #define LOG_TRACE(message) do {} while (0)
-    #define LOG_DEBUG(message) do {} while (0)
+
+#if CONFIG_ENABLE_LOGGING_INFO
+    #define LOG_INFO(message)  DO_ONCE(LOG4CXX_INFO (logger, message))
+#else
+    #define LOG_INFO(message)  DO_ONCE(;)
+#endif
+
+#if CONFIG_ENABLE_LOGGING_WARN
+    #define LOG_WARN(message)  DO_ONCE(LOG4CXX_WARN (logger, message))
+#else
+    #define LOG_WARN(message)  DO_ONCE(;)
+#endif
+
+#if CONFIG_ENABLE_LOGGING_FATAL
+    #define LOG_FATAL(message)  DO_ONCE(LOG4CXX_FATAL (logger, message))
+#else
+    #define LOG_FATAL(message)  DO_ONCE(;)
+#endif
+
+#if CONFIG_ENABLE_LOGGING_ERROR
+    #define LOG_ERROR(message)  DO_ONCE(LOG4CXX_ERROR (logger, message))
+#else
+    #define LOG_ERROR(message)  DO_ONCE(;)
+#endif
+
+#if CONFIG_ENABLE_LOGGING_TRACE
+    #define LOG_TRACE(message)  DO_ONCE(LOG4CXX_TRACE (logger, message))
+#else
+    #define LOG_TRACE(message)  DO_ONCE(;)
+#endif
+
+#if CONFIG_ENABLE_LOGGING_DEBUG
+    #define LOG_DEBUG(message)  DO_ONCE(LOG4CXX_DEBUG (logger, message))
+#else
+    #define LOG_DEBUG(message)  DO_ONCE(;)
 #endif
 
 #endif
