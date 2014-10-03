c3456c247829e4e869f1444f7f2576ab6ea00fbb
Alexey Ermakov <fufler@gmail.com>
2014-10-03 10:44:27 +0400

    Created separate header file for common used macros

--- src/libgcm/util/Assertion.hpp
+++ src/libgcm/util/Assertion.hpp
@@ -4,9 +4,8 @@
 #include <string>
 
 #include "libgcm/Exception.hpp"
+#include "libgcm/util/Macros.hpp"
 #include "libgcm/config.hpp"
-
-#define DO_WHILE(statement) do { statement } while(0)
 
 namespace std
 {
@@ -19,46 +18,46 @@ namespace std
 #if CONFIG_ENABLE_ASSERTIONS
 #define THROW_ASSERTION_FAILED(msg) THROW(gcm::Exception::UNKNOWN, msg)
 #else
-#define THROW_ASSERTION_FAILED(msg) DO_WHILE(;)
+#define THROW_ASSERTION_FAILED(msg) DO_ONCE(;)
 #endif
 
 
-#define assert_true(cond) DO_WHILE( \
+#define assert_true(cond) DO_ONCE( \
     if (!(cond)) \
         THROW_ASSERTION_FAILED("Condition evaluated to false instead of true. Condition: " #cond); \
     )
 
-#define assert_false(cond) DO_WHILE( \
+#define assert_false(cond) DO_ONCE( \
     if (cond) \
         THROW_ASSERTION_FAILED("Condition evaluated to true instead of false. Condition: " #cond); \
     )
 
-#define assert_ne(v1, v2) DO_WHILE( \
+#define assert_ne(v1, v2) DO_ONCE( \
     if ((v1) == (v2)) \
         THROW_ASSERTION_FAILED(std::string("Values " #v1 " and " #v2 " expected not to be equal, but both evaluate to ") + std::to_string(v1) + std::string(".")); \
     )
 
-#define assert_eq(v1, v2) DO_WHILE( \
+#define assert_eq(v1, v2) DO_ONCE( \
     if ((v1) != (v2)) \
         THROW_ASSERTION_FAILED(std::string("Values " #v1 " and " #v2 " expected to be equal, but " #v1 " evaluates to ") + std::to_string(v1) + std::string(" and " #v2 " evaluates to ") + std::to_string(v2) + std::string(".")); \
     )
 
-#define assert_gt(v1, v2) DO_WHILE( \
+#define assert_gt(v1, v2) DO_ONCE( \
     if ((v1) <= (v2)) \
         THROW_ASSERTION_FAILED(std::string("Value " #v1 " expected to be greater than " #v2 ", but " #v1 " evaluates to ") + std::to_string(v1) + std::string(" and " #v2 " evaluates to ") + std::to_string(v2) + std::string(".")); \
     )
 
-#define assert_ge(v1, v2) DO_WHILE( \
+#define assert_ge(v1, v2) DO_ONCE( \
     if ((v1) < (v2)) \
         THROW_ASSERTION_FAILED(std::string("Value " #v1 " expected to be greater than (or equal to) " #v2 ", but " #v1 " evaluates to ") + std::to_string(v1) + std::string(" and " #v2 " evaluates to ") + std::to_string(v2) + std::string(".")); \
     )
 
-#define assert_lt(v1, v2) DO_WHILE( \
+#define assert_lt(v1, v2) DO_ONCE( \
     if ((v1) >= (v2)) \
         THROW_ASSERTION_FAILED(std::string("Value " #v1 " expected to be less than " #v2 ", but " #v1 " evaluates to ") + std::to_string(v1) + std::string(" and " #v2 " evaluates to ") + std::to_string(v2) + std::string(".")); \
     )
 
-#define assert_le(v1, v2) DO_WHILE( \
+#define assert_le(v1, v2) DO_ONCE( \
     if ((v1) > (v2)) \
         THROW_ASSERTION_FAILED(std::string("Value " #v1 " expected to be less than (or equal to) " #v2 ", but " #v1 " evaluates to ") + std::to_string(v1) + std::string(" and " #v2 " evaluates to ") + std::to_string(v2) + std::string(".")); \
     )
---
+++ src/libgcm/util/Macros.hpp
@@ -0,0 +1,6 @@
+#ifndef MACROS_HPP
+#define MACROS_HPP 
+
+#define DO_ONCE(statement) do { statement } while(0)
+
+#endif /* MACROS_HPP */
