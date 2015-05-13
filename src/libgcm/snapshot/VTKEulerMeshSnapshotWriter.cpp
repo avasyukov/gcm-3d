868f4dac2f9486250e59af3baf2dd50ef3286f1b
Alexey Ermakov <fufler@gmail.com>
2015-05-13 12:08:11 +0300

    Fixed Euler mesh snapshotter (VTK node ordering differs from mesh internal one)

--- src/libgcm/snapshot/VTKEulerMeshSnapshotWriter.cpp
+++ src/libgcm/snapshot/VTKEulerMeshSnapshotWriter.cpp
@@ -6,6 +6,68 @@
 
 using namespace gcm;
 
+namespace gcm
+{
+    typedef struct
+    {
+        vector3u index;
+        vector3u dims;
+    } data_t;
+
+    template<>
+    MeshNodeIterator<EulerMesh, 0>::MeshNodeIterator(EulerMesh* mesh): mesh(mesh)
+    {
+        INIT_LOGGER("gcm.snapshot.MeshNodeIterator.EulerMesh");
+        data = new data_t;
+        static_cast<data_t*>(data)->dims = mesh->getNodeDimensions();
+    }
+
+    template<>
+    MeshNodeIterator<EulerMesh, 0>::~MeshNodeIterator()
+    {
+        delete static_cast<data_t*>(data);
+    }
+
+    template<>
+    bool MeshNodeIterator<EulerMesh, 0>::hasNext()
+    {
+        return static_cast<data_t*>(data)->index << static_cast<data_t*>(data)->dims;
+    }
+
+    template<>
+    MeshNodeIterator<EulerMesh, 0>& MeshNodeIterator<EulerMesh, 0>::operator++()
+    {
+        vector3u& v = static_cast<data_t*>(data)->index;
+        const vector3u& dims = static_cast<data_t*>(data)->dims;
+
+        v.x++;
+        if (v.x == dims.x)
+        {
+            v.x = 0;
+            v.y++;
+            if (v.y == dims.y)
+            {
+                v.y = 0;
+                v.z++;
+            }
+        }
+
+        return *this;
+    }
+
+    template<>
+    MeshNodeIterator<EulerMesh, 0>& MeshNodeIterator<EulerMesh, 0>::operator++(int)
+    {
+        return ++(*this);
+    }
+
+    template<>
+    CalcNode& MeshNodeIterator<EulerMesh, 0>::operator*()
+    {
+        return mesh->getNodeByEulerMeshIndex(static_cast<data_t*>(data)->index);
+    }
+};
+
 VTKEulerMeshSnapshotWriter::VTKEulerMeshSnapshotWriter()
 {
     INIT_LOGGER("gcm.snapshot.VTKEulerMeshSnapshotWriter");
