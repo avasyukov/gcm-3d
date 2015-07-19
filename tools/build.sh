3cc19116f623ec032932678fce1849537df30b24
Alexey Vasyukov <vasyukov@gmail.com>
2015-07-19 11:02:33 +0300

    Test tasks for this dead branch

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -R bin/ lib/ include/ share/
 ./waf distclean
-./waf configure --prefix=. --optimize
+./waf configure --prefix=. --optimize --without-tests --logging-level=info --disable-assertions
 ./waf build
 ./waf install
