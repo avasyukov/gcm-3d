4eda7d9d7a2cf60dcb4a94d4f6b62ee3c8cfa977
Alexey Ermakov <fufler@gmail.com>
2015-10-10 14:16:06 +0300

    Hardcoded yrange when plotting sensors data

--- tools/plot_sensors.py
+++ tools/plot_sensors.py
@@ -9,6 +9,7 @@ for (i,v) in enumerate(values):
     p = subprocess.Popen(['gnuplot'], stdin=subprocess.PIPE)
     script  = "set terminal pngcairo size 500,500 enhanced font 'Verdana,10';\n"
     script += "set output '%s.png';\n" % v
+    script += "set yrange [-5:+5];\n"
     script += "plot "
     for f in sys.argv[1:]:
         script += "'%s' using :%d with linespoints," % (f, 2+i)
