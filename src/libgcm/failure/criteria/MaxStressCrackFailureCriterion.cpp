e155860ac449497b9ea1b1b4c9af05aba1749387
RomanMikhailovDASR <ziroxxx3@gmail.com>
2025-04-12 14:07:01 +0300

    add laser markered task

--- src/libgcm/failure/criteria/MaxStressCrackFailureCriterion.cpp
+++ src/libgcm/failure/criteria/MaxStressCrackFailureCriterion.cpp
@@ -17,5 +17,8 @@ void MaxStressCrackFailureCriterion::checkFailure(ICalcNode& node, const float t
     auto props = mat->getFailureProperties();
     real limit = props[FAILURE_TYPE_MAX_STRESS][FAILURE_TYPE_MAX_STRESS_THRESHOLD];
     if (m_s[i_ms] > limit)
-        node.createCrack(i_ms);
+    {
+        // node.createCrack(i_ms);
+        node.setDestroyed(true);
+    }
 }
